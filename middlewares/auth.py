44f1ce0da752928ea58cf504a5ca47ea9ccbc37a
ownen <mr.joker.mr.joke@gmail.com>
2024-01-22 18:01:38 +0330

    Fix checking db in middleware

--- middlewares/auth.py
+++ middlewares/auth.py
@@ -1,16 +1,17 @@
-from fastapi import Request, HTTPException
+from fastapi import Request, HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer
 from sqlmodel.ext.asyncio.session import AsyncSession
 from connections import get_session
 from repository import TokenRepository
 from utils import token_validator
 from http import HTTPStatus
+from typing import Annotated
 
 
 oauth_schema = OAuth2PasswordBearer(tokenUrl="token")
 
 
-async def auth_user(request: Request, session: AsyncSession = get_session()):
+async def auth_user(request: Request, session: Annotated[AsyncSession, Depends(get_session)]):
     try:
         token = (
             request.headers["Authorization"]
